3c370800a4d575831aa299539dfebe0f2ba632d8
Jain01Pulkit <pj401624@gmail.com>
2024-06-17 10:15:05 +0530

    crud app using mongo

--- src/models/usermodel.rs
+++ src/models/usermodel.rs
@@ -1,9 +1,10 @@
-use sqlx::FromRow;
-use uuid::Uuid;
+use mongodb::bson::oid::ObjectId;
+use serde::{Serialize, Deserialize};
 
-#[derive(Debug, FromRow)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct User {
-    pub id: Option<Uuid>, 
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
     pub name: String,
     pub location: String,
     pub title: String,
